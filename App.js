db3af1c577702bc4fe7a3dd9d630ac27f60ccceb
Praveenmuthu503 <praveenmuthu@dorustree.in>
2022-07-11 10:59:14 +0530

    final db

--- App.js
+++ App.js
@@ -1,38 +1,38 @@
 const express = require("express");
-const App = express();
+const app = express();
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
-const Port = 5000;
-
-App.use(express.json()); //midilewhere
-const userdb = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
 const cors = require("cors");
-App.listen(Port, () => {
-  console.log(`"Good" Your App is Runnig GOOD ${Port}`);
-});
+const Port = 5000;
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true,
   optionSuccessStatus: 200,
 };
-App.use(cors(corsOptions));
-App.get("/", (req, res) => {
-  res.send("Learn in JWT");
+
+app.use(cors(corsOptions));
+app.use(express.json()); //midilewhere
+
+const userdb = JSON.parse(fs.readFileSync("./db.json", "utf-8"));
+app.listen(Port, () => {
+  console.log(`"Good" Your app is Runnig GOOD ${Port}`);
 });
-App.post("/checkAuth", (req, res) => {
+app.get("/", (req, res) => {
+  res.send(userdb);
+});
+app.post("/checkAuth", (req, res) => {
   checkAuth(req, res);
 });
-
 
 function isAuthenticated({ userMail, userPassword }) {
   return (
     userdb.users.findIndex(
       (user) => user.userMail === userMail && user.userPassword === userPassword
-    ) !== -1
-  );
+      ) !== -1
+      );
 }
 
-App.post("/register", (req, res) => {
+app.post("/register", (req, res) => {
   const { userMail, userPassword } = req.body;
   if (isAuthenticated({ userMail, userPassword })) {
     const status = 401;
@@ -64,23 +64,30 @@ App.post("/register", (req, res) => {
           res.status(status).json({ status, message });
         }
       }
-    );
+      );
+    });
+    res.status(200).json(req.body);
   });
-  res.status(200).json(req.body);
-});
+  
+  app.post("/login", (req, res) => {
+  console.log("=====================================",req)
 
-
-App.post("/login", (req, res) => {
-  const { userEmail,userPassword } = req.body;
-  const data = userdb.users.find((val)=>val.userMail === req.body.userEmail && val.userPassword === req.body.userPassword)
+  const { userEmail, userPassword } = req.body;
+  const data = userdb.users.find(
+    (val) =>
+      val.userMail === req.body.userEmail &&
+      val.userPassword === req.body.userPassword
+  );
   if (data === undefined) {
     res.status(400).json({
       status: 400,
       error: { msg: "Invalid credentials" },
     });
-  } else{
+  } else {
     const Token = jwt.sign({ userEmail }, "hdhdhdhd", { expiresIn: "5m" });
-    const refreshToken = jwt.sign({ userEmail }, "hdhdhdhd", { expiresIn: "1h" });
+    const refreshToken = jwt.sign({ userEmail }, "hdhdhdhd", {
+      expiresIn: "1h",
+    });
     console.log(Token, "get login token");
     res.status(200).json({
       status: "success",
@@ -92,7 +99,7 @@ App.post("/login", (req, res) => {
 const checkAuth = (req, res, next) => {
   const { TokenExpiredError } = jwt;
   const catchError = (error, res) => {
-    console.log("errorShow", error,res);
+    console.log("errorShow", error, res);
     if (error instanceof TokenExpiredError) {
       return res
         .status(401)
